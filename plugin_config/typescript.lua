08090e0250e910071a54a5f9ad345fe2e90a24db
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-02-06 07:35:39 +0800

    Use the LSP instead.

--- plugin_config/typescript.lua
+++ plugin_config/typescript.lua
@@ -1,29 +1,3 @@
-require("lspconfig").tsserver.setup({})
-
---[=====[
-Use VS Code for Web Development
-
-vim.api.nvim_exec(
-	[[
-  autocmd BufEnter *.js,*.jsx,*.ts,*.tsx,*.json call coc#activate()
-  autocmd BufLeave *.js,*.jsx,*.ts,*.tsx,*.json call coc#deactivate()
-  inoremap <silent><expr> <c-space> coc#refresh()
-  nmap <silent> gd <Plug>(coc-definition)
-  nmap <silent> gy <Plug>(coc-type-definition)
-  nmap <silent> gi <Plug>(coc-implementation)
-  nmap <silent> gr <Plug>(coc-references)
-  nnoremap <silent> K :call v:lua.show_documentation()<CR>
-]],
-	false
-)
-
--- Temporary solution for handling documentation.
-local function show_documentation()
-	local filetype = vim.bo.filetype
-	if vim.tbl_contains({ "vim", "help" }, filetype) then
-		vim.cmd("h " .. vim.fn.expand("<cword>"))
-	else
-		vim.fn.CocActionAsync("doHover")
-	end
-end
---]=====]
+require("lspconfig").tsserver.setup({
+	cmd = { "typescript-language-server", "--stdio" },
+})
