f87dc5640572b59a8d8d275bf95ec71bbfc4d72c
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-02-06 08:17:29 +0800

    Update typescript config.

--- plugin_config/typescript.lua
+++ plugin_config/typescript.lua
@@ -1,3 +1,18 @@
 require("lspconfig").tsserver.setup({
 	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = { "typescript", "typescriptreact" },
+	init_options = {
+		documentFormatting = true,
+	},
+	settings = {
+		rootMarkers = { "package.json" },
+		languages = {
+			typescript = {
+				{
+					formatCommand = "efm-langserver",
+					formatStdin = true,
+				},
+			},
+		},
+	},
 })
