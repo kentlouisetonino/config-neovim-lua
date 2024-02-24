c5c9b931d860dfb9907a7edf2347de118bbb1983
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-02-24 10:35:01 +0800

    Temporarily comment out the auto open nerdtree.

--- plugin_config/nerdtree.lua
+++ plugin_config/nerdtree.lua
@@ -5,16 +5,16 @@ vim.api.nvim_set_var("NERDTreeShowHidden", 1)
 vim.api.nvim_set_keymap("n", "<C-t>", ":NERDTreeToggle<CR>", { noremap = true, silent = true })
 
 -- Start NERDTree. If a file is specified, move the cursor to its window.
-vim.api.nvim_exec(
-	[[
-  augroup NERDTreeAutoCmd
-    autocmd!
-    autocmd StdinReadPre * let s:std_in=1
-    autocmd VimEnter * NERDTree | if argc() > 0 || exists("s:std_in") | wincmd p | endif | silent NERDTreeFind | wincmd p
-  augroup END
-]],
-	false
-)
+--vim.api.nvim_exec(
+--	[[
+--  augroup NERDTreeAutoCmd
+--    autocmd!
+--    autocmd StdinReadPre * let s:std_in=1
+--    autocmd VimEnter * NERDTree | if argc() > 0 || exists("s:std_in") | wincmd p | endif | silent NERDTreeFind | wincmd p
+--  augroup END
+--]],
+--	false
+--)
 
 -- Backup for nerdtree autosynchronize.
 vim.api.nvim_set_keymap("n", "<F2>", ":NERDTreeFind<CR>:wincmd p<CR>", { noremap = true, silent = true })
