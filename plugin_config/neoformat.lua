3876ab518d1ecba1f4aa8ee55e8df00895302199
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-03-25 20:52:52 +0800

    Include support for C informatting.

--- plugin_config/neoformat.lua
+++ plugin_config/neoformat.lua
@@ -2,4 +2,4 @@
 vim.g.neoformat_try_node_exe = 1
 
 -- Format on save.
-vim.api.nvim_exec([[autocmd BufWritePre *.js,*.json,*.ts,*.tsx*,*.jsx,*.html,*.css,*.dart Neoformat]], false)
+vim.api.nvim_exec([[autocmd BufWritePre *.js,*.json,*.ts,*.tsx*,*.jsx,*.html,*.css,*.dart,*.c Neoformat]], false)
