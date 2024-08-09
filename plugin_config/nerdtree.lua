1b13b909ca298d08ff7d20bb959942ca6b84e9b1
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-09 17:14:18 +0800

    Close nerd tree by default.

--- plugin_config/nerdtree.lua
+++ plugin_config/nerdtree.lua
@@ -5,7 +5,7 @@ vim.api.nvim_set_var("NERDTreeShowHidden", 1)
 vim.api.nvim_set_keymap("n", "<C-t>", ":NERDTreeToggle<CR>:wincmd p<CR>", { noremap = true, silent = true })
 
 -- Automatically open NERDTree when Neovim starts (optional)
-vim.cmd([[autocmd VimEnter * NERDTree]])
+-- vim.cmd([[autocmd VimEnter * NERDTree]])
 
 -- Backup for nerdtree autosynchronize.
 vim.api.nvim_set_keymap("n", "<F3>", ":NERDTreeFind<CR>:wincmd p<CR>", { noremap = true, silent = true })
