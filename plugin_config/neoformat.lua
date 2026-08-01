03a97c8b56dde307fd93b1551aa22aa3d8adf4b7
Kent Tonino <kenttonino.io@gmail.com>
2026-08-01 12:24:22 +0800

    feat: lua files formatter

--- plugin_config/neoformat.lua
+++ plugin_config/neoformat.lua
@@ -3,6 +3,6 @@ vim.g.neoformat_try_node_exe = 1
 
 -- Format on save.
 vim.api.nvim_exec(
-	[[autocmd BufWritePre *.js,*.json,*.ts,*.tsx*,*.jsx,*.html,*.css,*.dart,*.c,*.cpp,*.ino,*.java,*.py,*.s,*.prisma Neoformat]],
+	[[autocmd BufWritePre *.js,*.json,*.ts,*.tsx*,*.jsx,*.html,*.css,*.dart,*.c,*.cpp,*.ino,*.java,*.py,*.s,*.prisma,*.lua Neoformat]],
 	false
 )
